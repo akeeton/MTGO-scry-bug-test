cea64d934ca96b976a1b314ec6884d00a4052093
Andrew Keeton <andrewrkeeton@gmail.com>
2017-10-17 03:03:10 -0400

    Go from clicking "Play" to scrying to the bottom

--- MTGO-scry-bug-test.sikuli/MTGO-scry-bug-test.py
+++ MTGO-scry-bug-test.sikuli/MTGO-scry-bug-test.py
@@ -1,9 +1,15 @@
-if not find("1508222046889.png"):
-    exit()
+click("1508223092723.png")
+
+wait("1508222046889.png", 5)
 
 click("1508222069014.png")
 
-if not find("1508222375762.png"):
-    exit()
+wait("1508223167943.png", 5)
 
 click("1508222123880.png")
+
+wait("1508222653397.png", 5)
+
+click(Location(1261, 356))
+
+click("scry-to-bottom.png")
