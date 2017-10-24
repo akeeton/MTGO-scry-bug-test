d1d3a8d5729dda7ca32d140dfcf078e9afbb57a4
Andrew Keeton <andrewrkeeton@gmail.com>
2017-10-24 10:25:24 -0400

    Move configurable constants back to the top level

--- MTGO-scry-bug-test.sikuli/MTGO-scry-bug-test.py
+++ MTGO-scry-bug-test.sikuli/MTGO-scry-bug-test.py
@@ -4,6 +4,23 @@ import shutil
 import tempfile
 import time
 
+REGION_PLAY                                         = Region(7,965,334,57)
+REGION_MULLIGAN_KEEP                                = Region(0,13,175,154)
+REGION_CARD_SENT_TO_BOTTOM                          = Region(1209,283,102,63)
+REGION_TEMPORARY_ZONE                               = Region(1017,199,124,29)
+REGION_PUT_ON_THE_BOTTOM_OF_YOUR_LIBRARY            = Region(921,181,459,166)
+REGION_CHAT_PUT_A_CARD_ON_THE_BOTTOM_OF_THE_LIBRARY = Region(1501,671,398,51)
+REGION_CARD_PREVIEW_CAPTURE                         = Region(1549,546,52,23)
+REGION_CONCEDE_MATCH_BUTTON                         = Region(891,554,133,48)
+
+LOCATION_PLAY                                       = Location(169, 995)
+LOCATION_MULLIGAN                                   = Location(47, 141)
+LOCATION_TEMPORARY_ZONE_CARD                        = Location(1195, 382)
+LOCATION_PUT_ON_THE_BOTTOM_OF_YOUR_LIBRARY          = Location(1118, 430)
+LOCATION_FIRST_CARD_IN_HAND                         = Location(282, 910)
+LOCATION_X_CLOSE                                    = Location(1902, 14)
+LOCATION_CONCEDE_MATCH                              = Location(961, 579)
+
 def hash_file(file_path):
     hasher = hashlib.md5()
     with open(file_path, 'rb') as opened_file:
@@ -13,23 +30,6 @@ def hash_file(file_path):
     return hasher.hexdigest()
 
 def main():
-    REGION_PLAY                                         = Region(7,965,334,57)
-    REGION_MULLIGAN_KEEP                                = Region(0,13,175,154)
-    REGION_CARD_SENT_TO_BOTTOM                          = Region(1209,283,102,63)
-    REGION_TEMPORARY_ZONE                               = Region(1017,199,124,29)
-    REGION_PUT_ON_THE_BOTTOM_OF_YOUR_LIBRARY            = Region(921,181,459,166)
-    REGION_CHAT_PUT_A_CARD_ON_THE_BOTTOM_OF_THE_LIBRARY = Region(1501,671,398,51)
-    REGION_CARD_PREVIEW_CAPTURE                         = Region(1549,546,52,23)
-    REGION_CONCEDE_MATCH_BUTTON                         = Region(891,554,133,48)
-
-    LOCATION_PLAY                                       = Location(169, 995)
-    LOCATION_MULLIGAN                                   = Location(47, 141)
-    LOCATION_TEMPORARY_ZONE_CARD                        = Location(1195, 382)
-    LOCATION_PUT_ON_THE_BOTTOM_OF_YOUR_LIBRARY          = Location(1118, 430)
-    LOCATION_FIRST_CARD_IN_HAND                         = Location(282, 910)
-    LOCATION_X_CLOSE                                    = Location(1902, 14)
-    LOCATION_CONCEDE_MATCH                              = Location(961, 579)
-
     TEMP_DIR_PREFIX = time.strftime("MTGO-scry-bug_%Y-%m-%d_%H-%M-%S", time.gmtime())
     TEMP_PATH = tempfile.mkdtemp(prefix=TEMP_DIR_PREFIX)
     print "TEMP_PATH:", TEMP_PATH
